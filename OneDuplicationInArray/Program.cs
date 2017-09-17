ab8b35b4cad634b44ad80cf660bf34cb96c23bc9
Richard Iyama <richardiyama@yahoo.com>
2017-09-17 13:01:36 +0100

    initial commit

---
+++ OneDuplicationInArray/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneDuplicationInArray
+{
+    class Program
+    {
+
+       static int Duplicate(int[] numbers)
+        {
+            int length = numbers.Length;
+            int sum1 = 0;
+            for (int i = 0; i < length; ++i)
+            {
+                if (numbers[i] < 0 || numbers[i] > length - 2)
+                    throw new ArgumentException("Invalid numbers.");
+                sum1 += numbers[i];
+            }
+            int sum2 = ((length - 1) * (length - 2)) >> 1;
+            return sum1 - sum2;
+        }
+        static void Main(string[] args)
+        {
+       
+            int[] array = new int[] {0, 2,1, 3, 2};
+            
+            int duplicate;
+           
+         
+
+            duplicate = Duplicate(array);
+            System.Console.WriteLine("The duplicate array is: " + duplicate);
+
+        }
+    }
+}
